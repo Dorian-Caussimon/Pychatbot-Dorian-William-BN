5722a16844c827c9af12bf8ba7502ea5218b1398
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-17 09:17:21 +0100

    new

--- fonction.py
+++ fonction.py
@@ -10,10 +10,12 @@ def cleen (files_names):
         F=[]
         with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
             F = str(f1.readlines())
-
+# division des mot en caractère
         for val in F:
             Z = []
             v = list(val)
             Z.append(v)
+# vérification des caractère
             for val in Z:
-                if ord(val) <= 90 and ord(val) <=
+                caract = ord(val)
+                if caract <= 90 and caract >= 65:
