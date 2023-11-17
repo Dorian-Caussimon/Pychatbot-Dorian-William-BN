953931fa97d184af51038a52aca0442629eec661
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-17 10:53:50 +0100

    new

--- fonction.py
+++ fonction.py
@@ -1,4 +1,5 @@
 import os
+ponctuation = [',','.','!','?']
 def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
@@ -8,14 +9,15 @@ def list_of_files(directory, extension):
 def cleaned (files_names):
     for val in files_names:
         F=[]
-        F1 = []
         with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
             F = str(f1.readlines())
-            F1 = [x.replace('\n','') for x in F]
-            print F1
 # division des mot en caractère
         for val in F:
             Z = []
             Z.append(val)
-# vérification des caractèr
-
+            for val in Z:
+                caractere_ascii = ord(val)
+                #majuscule --> minuscule
+                if caractere_ascii >= 65 and caractere_ascii <=90:
+                    caractere_ascii += 32
+                    caractere = chr(caractere_ascii)
