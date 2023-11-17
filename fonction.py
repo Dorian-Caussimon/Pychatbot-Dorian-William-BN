bc490091544c5573d3d7d080dec06d5828e19c25
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-17 10:14:12 +0100

    new

--- fonction.py
+++ fonction.py
@@ -5,15 +5,17 @@ def list_of_files(directory, extension):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
-def cleen (files_names):
+def cleaned (files_names):
     for val in files_names:
         F=[]
+        F1 = []
         with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
             F = str(f1.readlines())
-# Division des mots en caractères
+            F1 = [x.replace('\n','') for x in F]
+            print F1
+# division des mot en caractère
         for val in F:
             Z = []
             Z.append(val)
-            print(Z,end='')
+# vérification des caractèr
 
-# Vérification des caractères
