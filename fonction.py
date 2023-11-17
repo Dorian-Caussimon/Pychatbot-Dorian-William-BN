8aa278aab475f1123c74def98364536a408740a7
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-17 11:07:49 +0100

    new

--- fonction.py
+++ fonction.py
@@ -8,16 +8,22 @@ def list_of_files(directory, extension):
     return files_names
 def cleaned (files_names):
     for val in files_names:
-        F=[]
+        F =[]
         with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
-            F = str(f1.readlines())
-# division des mot en caractère
-        for val in F:
-            Z = []
-            Z.append(val)
-            for val in Z:
+            ligne = f.readline()
+            while ligne != "":
+                print(ligne)
+            ligne = f.readline()
+
+    # division des mot en caractère
+    for val in F:
+        Z = []
+        Z.append(val)
+        for val in Z:
+            with open('./cleaned/{}'.format(val), 'a', encoding="utf-8") as f1:
                 caractere_ascii = ord(val)
-                #majuscule --> minuscule
-                if caractere_ascii >= 65 and caractere_ascii <=90:
-                    caractere_ascii += 32
-                    caractere = chr(caractere_ascii)
+            # majuscule --> minuscule
+            if caractere_ascii >= 65 and caractere_ascii <= 90:
+                caractere_ascii += 32
+                caractere = chr(caractere_ascii)
+
