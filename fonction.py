03f4f125477b4e5df8fbdc2cc5e84be0e47c2f60
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-17 09:12:09 +0100

    new

--- fonction.py
+++ fonction.py
@@ -5,3 +5,15 @@ def list_of_files(directory, extension):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
+def cleen (files_names):
+    for val in files_names:
+        F=[]
+        with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
+            F = str(f1.readlines())
+
+        for val in F:
+            Z = []
+            v = list(val)
+            Z.append(v)
+            for val in Z:
+                if ord(val) <= 90 and ord(val) <=
--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from fonction import *
 # permet d'avoir la liste des fichier
 directory = "./speeches"
 files_names = list_of_files(directory, "txt")
-print(files_names)
 
 #netoyage
-for val in files_names:
-    with open ('./speeches/{}'.format(val),'r',encoding="utf-8" ) as f1:
-        F  = str(f1.readlines())
+cleen(files_names)
