654659720aa83310c2e3c797e552093d2163b997
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-18 09:10:17 +0100

    new

--- fonction.py
+++ fonction.py
@@ -1,29 +1,57 @@
 import os
-ponctuation = [',','.','!','?']
+#
+# Liste pour les ponctuation
+ponctuation = ["," , "." , "!" , ":" , "?", ";"]
+
 def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
+
 def cleaned (files_names):
-    for val in files_names:
-        F =[]
-        with open('./speeches/{}'.format(val), 'r', encoding="utf-8") as f1:
-            ligne = f.readline()
-            while ligne != "":
-                print(ligne)
-            ligne = f.readline()
+    for nom in files_names:
+        # ouvre le ficher texte a nétoyer
+        with open('./speeches/{}'.format(nom), 'r', encoding="utf-8") as f1:
+            F = f1.readlines()
 
-    # division des mot en caractère
-    for val in F:
-        Z = []
-        Z.append(val)
-        for val in Z:
-            with open('./cleaned/{}'.format(val), 'a', encoding="utf-8") as f1:
-                caractere_ascii = ord(val)
-            # majuscule --> minuscule
-            if caractere_ascii >= 65 and caractere_ascii <= 90:
-                caractere_ascii += 32
-                caractere = chr(caractere_ascii)
+            # crée le nouveaux ficher pour le texte netoyer
+            with open('./cleaned/{}'.format(nom), 'w', encoding="utf-8") as f2:
 
+                # divise les txt en ligne
+                for ligne in F:
+
+                    # divise les ligne en lettre
+                    for lettre in ligne:
+                        l_ascii = ord(lettre)
+
+                        # majuscule --> minuscule
+                        if l_ascii >= 65 and l_ascii <= 90:
+                            l_ascii += 32
+                            new_lettre = chr(l_ascii)
+                            f2.write(new_lettre)
+                        # pour les minuscule
+                        elif l_ascii >=97 and l_ascii <= 122:
+                            f2.write(lettre)
+                        # cas particuler pour les è ; é ; ê
+                        elif l_ascii >= 232 and l_ascii <= 234:
+                            f2.write('e')
+
+                        # cas particulier â et à
+                        elif l_ascii == 224 or l_ascii == 226:
+                            f2.write('a')
+
+                        # cas particulier û et ù
+                        elif l_ascii == 251 or l_ascii == 249:
+                            f2.write('u')
+
+                        # pour les espace tiret et apostroph
+                        elif lettre == " " or lettre == "-" or lettre == "'" :
+                            f2.write(" ")
+                        # pour la pnctuation
+                        else:
+                            for ponct in ponctuation:
+                                if ponct == lettre:
+                                    f2.write('')
+                    f2.write("\n")
