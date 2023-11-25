6d279ab2459847e2afcf5267dad630247000379f
Dorian caussimon <Dorian.caussimon.lucas@gmail.com>
2023-11-25 11:26:43 +0100

    IDF TF

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ files_names = list_of_files(directory,'txt')
 # Nettoyage des fichiers .txt
 cleaned(files_names)
 
-# Recherche de la fréquence de chaque mot dans le .txt
-TF(files_names)
+# méthode TF_IDF
+TF_IDF(files_names)
+IDF(files_names)
